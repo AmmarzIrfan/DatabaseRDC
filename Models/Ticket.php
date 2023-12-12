f2a63f402e4c1919c22a9c8335fd21f12f0ad950
AmmarzIrfan <150988664+AmmarzIrfan@users.noreply.github.com>
2023-12-12 23:54:00 +0800

    Add files via upload

---
+++ Models/Ticket.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    use HasFactory;
+    protected $table='tickets';
+    protected $fillable = [
+        'cust_id',
+        'ticket_type',
+        'purchase_date',
+        'validity_start',
+        'validity_end',
+        'status',
+        'price',
+        'payment_status',
+
+    ];
+    public function visitor()
+    {
+        return $this->belongsTo(VisitorProfile::class, 'cust_id', 'id');
+    }
+    public function accesslog()
+    {
+        return $this->hasMany(AccessLog::class, 'ticket_id', 'id');
+    }
+    }
---
+++ Models/VisitorProfile.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VisitorProfile extends Model
+{
+    use HasFactory;
+    protected $table = 'visitor_profiles';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'residency',
+
+    ];
+
+    public function ticket()
+    {
+        return $this->hasOne(Ticket::class, 'cust_id', 'id');
+    }
+    public function accesslog()
+    {
+        return $this->hasMany(AccessLog::class, 'cust_id', 'id');
+    }
+
+}
